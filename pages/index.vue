290f822f805d766587d8433710633b12b24f14bf
CarEnthusiastJohn50 <129770852+pow4you@users.noreply.github.com>
2023-09-26 20:28:44 +0200

    custom Pride flag component
    This component displays a specified prideflag

--- pages/index.vue
+++ pages/index.vue
@@ -4,8 +4,8 @@
     <div class="p-4">
       <section>
         <h1 class="text-center pb-4"><Icon name="ph:sparkle"></Icon>Hi_ I'm Pow<Icon name="ph:sparkle"></Icon> </h1>
-        <p class="sm:px-12 md:px-24 text-center text-lg"><Icon name="ph:caret-double-right-bold"></Icon> Full-Stack Developer / Quantum Engineer</p>
-        <p class="sm:px-12 md:px-24 text-center text-lg"><Icon name="ph:caret-double-right-bold"></Icon> Vue <Icon name="file-icons:vue"></Icon>, React <Icon name="simple-icons:react"></Icon>, Tailwind <Icon name="simple-icons:tailwindcss"></Icon>, Qiskit <Icon name="simple-icons:qiskit"></Icon></p>
+        <p class="text-center text-lg max-sm:text-base"><Icon name="ph:caret-double-right-bold"></Icon> Full-Stack Developer / Quantum Engineer</p>
+        <p class="text-center text-lg max-sm:text-base"><Icon name="ph:caret-double-right-bold"></Icon> Vue <Icon name="file-icons:vue"></Icon>, React <Icon name="simple-icons:react"></Icon>, Tailwind <Icon name="simple-icons:tailwindcss"></Icon>, Qiskit <Icon name="simple-icons:qiskit"></Icon></p>
 
       </section>
     </div>
@@ -38,23 +38,33 @@
           :description="showcase[1].description"
         ></SectionSocialLarge>
         
-        <section class="col-span-5 col-start-1 max-sm:col-span-12 transition-all py-4">
+        <section class="col-span-4 col-start-1 max-sm:col-span-12 transition-all py-4">
           <h1 class="text-center">Socials</h1>
           <p class="text-justify  sm:pr-2">
             This is my list of social media links. Basically everywhere I have an account that may be active or inactive, as well as Project pages I've been working on. So like basically my life lol.
           </p>
+          <br />
+          
+          <span class="max-sm:hidden">
+          
+            <h2 class="text-center">Spotlight</h2>
+            <p class="text-justify  sm:pr-2">
+              social-media post spotlight here.
+            </p>
+            
+          </span>
         </section>
         
-        <section class="col-span-7 col-start-6 max-sm:col-span-12 max-sm:col-start-1 transition-all py-2">
+        <section class="col-span-8 col-start-5 max-sm:col-span-12 max-sm:col-start-1 transition-all py-2">
           <div 
           v-for="(section, name) in links"
           :key="name"
           
           class="
             
-            flex flex-col gap-2 py-2
+            flex flex-col gap-2 py-2 lg:grid lg:grid-cols-2
           ">
-            <h2 v-if="name != 'showcase'" class="text-center">{{ useCapitalize(name) }}</h2>
+            <h2 v-if="name != 'showcase'" class="text-center col-span-2">{{ useCapitalize(name) }}</h2>
             
             <SectionSocialSmall
               v-if="name != 'showcase'"
@@ -78,9 +88,28 @@
     </SectionCard>
     
     <SectionCard CardIcon="ph:sparkle-bold" CardTitle="Pronounce" id="Pronouns">
-      <div class="p-4 gap-2 grid grid-cols-12 auto-rows-auto">
-        <section class="col-span-4 col-start-1 transition-all"></section>
-        <section class="col-span-8 col-start-5 transition-all pb-4">
+      <div class="p-4 gap-2 grid grid-cols-12 auto-rows-auto ">
+        <section class="flex flex-col col-span-4 max-sm:col-span-5 transition-all gap-2">
+          <h1 class="text-center">Who's Pow?</h1>
+          
+          <SectionPronounsPrideflag
+            class="h-5"
+            flag="lesbian"
+          ></SectionPronounsPrideflag>
+          
+          <p class="text-justify  sm:pr-2">
+            Pow is a 21 year old transbian that lives in Austria. 
+          </p>
+          
+          <SectionPronounsPrideflag
+            class="h-5"
+            flag="trans"
+          ></SectionPronounsPrideflag>
+          
+          //Donate button//
+          
+        </section>
+        <section class="col-span-8 max-sm:col-span-7 transition-all pb-4">
           <div
             v-for="(section, name) in pronouns"
             :key="name"
