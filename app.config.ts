e9c11618894993c53a95e4f50f04e41d2304a452
CarEnthusiastJohn50 <129770852+pow4you@users.noreply.github.com>
2024-03-09 10:19:37 +0100

    Update app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -6,7 +6,7 @@ export default defineAppConfig({
         {path: "/", title: "Home", icon: "ph:house-light", active_icon: "ph:house-bold"},
         {path: "/#Pronouns", title: "Pronouns", icon: "ph:sparkle-light", active_icon: "ph:sparkle-bold"},
         {path: "/#Socials", title: "Socials", icon: "ph:heart-light", active_icon: "ph:heart-bold"},
-        {path: "/about/", title: "About", icon: "ph:book-open-text-light", active_icon: "ph:book-open-text-bold"},
+        // {path: "/about/", title: "About", icon: "ph:book-open-text-light", active_icon: "ph:book-open-text-bold"},
     ],
     
     pronouns : pronouns,
