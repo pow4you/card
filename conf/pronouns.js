fb191f3f30acac9483add045da1f0676671a4b7a
Morplson <129770852+pow4you@users.noreply.github.com>
2024-03-09 13:13:18 +0100

    Update pronouns.js

--- conf/pronouns.js
+++ conf/pronouns.js
@@ -15,7 +15,7 @@ export default
                 "Vrizzy"
             ],
             "Aliases": [
-                "Danger", 
+                "Love", 
                 "Nghariad"
             ]
         }
