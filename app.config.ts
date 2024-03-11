d2a717f8064642f821eaef3507eefcc1b5c0362c
Morplson <129770852+pow4you@users.noreply.github.com>
2024-03-11 16:28:02 +0100

    Update app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -7,6 +7,7 @@ export default defineAppConfig({
         {path: "/#Pronouns", title: "Pronouns", icon: "ph:sparkle-light", active_icon: "ph:sparkle-bold"},
         {path: "/#Socials", title: "Socials", icon: "ph:heart-light", active_icon: "ph:heart-bold"},
         {path: "/about/", title: "About", icon: "ph:book-open-text-light", active_icon: "ph:book-open-text-bold"},
+        {path: "https://gofund.me/863ebe94", title: "FFS Fund", icon: "uil:medical", active_icon: "uil:medical"},
     ],
     
     pronouns : pronouns,
